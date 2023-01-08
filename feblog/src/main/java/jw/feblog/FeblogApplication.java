e074b603f0ada162cba6be6b9ecae797f9dd0c2c
정재원 <wjdwodnjs23@naver.com>
2023-01-08 18:02:32 +0900

    Initial commit

---
+++ feblog/src/main/java/jw/feblog/FeblogApplication.java
@@ -0,0 +1,13 @@
+package jw.feblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeblogApplication.class, args);
+	}
+
+}
---
+++ feblog/src/test/java/jw/feblog/FeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package jw.feblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
